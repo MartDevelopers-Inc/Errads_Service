5cd33afb47ac07b06cb775e3dd4d6db37ddc0f61
Martin Mbithi <martdevelopers254@gmail.com>
2022-03-15 17:05:37 +0300

    Initial Project Set Up Intergrate Lite Devlan Framework

---
+++ config/checklogin.php
@@ -0,0 +1,12 @@
+<?php
+
+function check_login()
+{
+if ((strlen($_SESSION['login_id']) == 0)) {
+		$host = $_SERVER['HTTP_HOST'];
+		$uri  = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+		$extra = "index";
+		$_SESSION["login"] = "";
+		header("Location: http://$host$uri/$extra");
+	}
+}
