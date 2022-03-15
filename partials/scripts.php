afddf8320a8f3876a085a51c079afc9b57e874c4
Martin Mbithi <martdevelopers254@gmail.com>
2022-03-15 17:11:56 +0300

    Register Assets

---
+++ partials/scripts.php
@@ -0,0 +1,9 @@
+<!-- ========= JS Files =========  -->
+<!-- Bootstrap -->
+<script src="../assets/js/lib/bootstrap.bundle.min.js"></script>
+<!-- Ionicons -->
+<script type="module" src="../../unpkg.com/ionicons%405.5.2/dist/ionicons/ionicons.js"></script>
+<!-- Splide -->
+<script src="../assets/js/plugins/splide/splide.min.js"></script>
+<!-- Base Js File -->
+<script src="../assets/js/base.js"></script>
