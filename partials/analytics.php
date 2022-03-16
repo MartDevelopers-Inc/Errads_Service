ca03a7dd3f2cb96968261c883f238610caee6cd5
Martin Mbithi <martdevelopers254@gmail.com>
2022-03-16 08:31:44 +0300

    Craft Application Analytics

--- partials/analytics.php
+++ partials/analytics.php
@@ -20,48 +20,149 @@
  * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+/* Sessions */
+$login_id = $_SESSION['login_id'];
+$user_access_level = $_SESSION['login_rank'];
 
-/* Freelancers */
-$query = "SELECT COUNT(*)  FROM users u 
-INNER JOIN login l ON l.login_id = u.user_login_id
-WHERE login_rank = 'Freelancer' ";
-$stmt = $mysqli->prepare($query);
-$stmt->execute();
-$stmt->bind_result($Freelancers);
-$stmt->fetch();
-$stmt->close();
+if ($user_access_level == 'Administrator') {
+    /* Load Admin Analytics */
+
+    /* Freelancers */
+    $query = "SELECT COUNT(*)  FROM users u 
+    INNER JOIN login l ON l.login_id = u.user_login_id
+    WHERE login_rank = 'Freelancer' ";
+    $stmt = $mysqli->prepare($query);
+    $stmt->execute();
+    $stmt->bind_result($freelancers);
+    $stmt->fetch();
+    $stmt->close();
 
 
-/* CLients */
-$query = "SELECT COUNT(*)  FROM users u 
-INNER JOIN login l ON l.login_id = u.user_login_id
-WHERE login_rank = 'Client' ";
-$stmt = $mysqli->prepare($query);
-$stmt->execute();
-$stmt->bind_result($Clients);
-$stmt->fetch();
-$stmt->close();
+    /* CLients */
+    $query = "SELECT COUNT(*)  FROM users u 
+    INNER JOIN login l ON l.login_id = u.user_login_id
+    WHERE login_rank = 'Client' ";
+    $stmt = $mysqli->prepare($query);
+    $stmt->execute();
+    $stmt->bind_result($clients);
+    $stmt->fetch();
+    $stmt->close();
 
-/* Posted Errands */
-$query = "SELECT COUNT(*)  FROM errands";
-$stmt = $mysqli->prepare($query);
-$stmt->execute();
-$stmt->bind_result($errands);
-$stmt->fetch();
-$stmt->close();
+    /* Posted Errands */
+    $query = "SELECT COUNT(*)  FROM errands";
+    $stmt = $mysqli->prepare($query);
+    $stmt->execute();
+    $stmt->bind_result($errands);
+    $stmt->fetch();
+    $stmt->close();
 
-/* Biddings */
-$query = "SELECT COUNT(*)  FROM biddings";
-$stmt = $mysqli->prepare($query);
-$stmt->execute();
-$stmt->bind_result($biddings);
-$stmt->fetch();
-$stmt->close();
+    /* Biddings */
+    $query = "SELECT COUNT(*)  FROM biddings";
+    $stmt = $mysqli->prepare($query);
+    $stmt->execute();
+    $stmt->bind_result($biddings);
+    $stmt->fetch();
+    $stmt->close();
 
-/* Total Amount Spent */
-$query = "SELECT SUM(payment_amount)  FROM payments";
-$stmt = $mysqli->prepare($query);
-$stmt->execute();
-$stmt->bind_result($payments);
-$stmt->fetch();
-$stmt->close();
+    /* Total Amount Spent */
+    $query = "SELECT SUM(payment_amount)  FROM payments";
+    $stmt = $mysqli->prepare($query);
+    $stmt->execute();
+    $stmt->bind_result($payments);
+    $stmt->fetch();
+    $stmt->close();
+} else if ($user_access_level == 'Client') {
+    /* Client  Analytics */
+    $ret = "SELECT * FROM users WHERE user_login_id = '$login_id'";
+    $stmt = $mysqli->prepare($ret);
+    $stmt->execute(); //ok
+    $res = $stmt->get_result();
+    while ($client = $res->fetch_object()) {
+        $client_id = $client_user_id;
+        /* Freelancers */
+        $query = "SELECT COUNT(*)  FROM users u 
+        INNER JOIN login l ON l.login_id = u.user_login_id
+        WHERE login_rank = 'Freelancer' ";
+        $stmt = $mysqli->prepare($query);
+        $stmt->execute();
+        $stmt->bind_result($freelancers);
+        $stmt->fetch();
+        $stmt->close();
+
+        /* Posted Errands */
+        $query = "SELECT COUNT(*)  FROM errands WHERE errand_user_id = '$client_id' ";
+        $stmt = $mysqli->prepare($query);
+        $stmt->execute();
+        $stmt->bind_result($errands);
+        $stmt->fetch();
+        $stmt->close();
+
+
+        /* My Expenditure  */
+        $query = "SELECT SUM(payment_amount)  FROM payments p
+        INNER JOIN accepted_bids ab ON ab.accepted_bid_id = p.payment_accepted_bid_id
+        INNER JOIN biddings b ON b.bidding_id = ab.accepted_bid_bidding_id 
+        WHERE b.bidding_errand_id = '$client_id' ";
+        $stmt = $mysqli->prepare($query);
+        $stmt->execute();
+        $stmt->bind_result($payments);
+        $stmt->fetch();
+        $stmt->close();
+    }
+} else {
+    /* Freelancer Analytics */
+    $ret = "SELECT * FROM users WHERE user_login_id = '$login_id'";
+    $stmt = $mysqli->prepare($ret);
+    $stmt->execute(); //ok
+    $res = $stmt->get_result();
+    while ($freelancer = $res->fetch_object()) {
+        $freelancer_id = $freelancer->user_id;
+        /* Client Analytics */
+        $query = "SELECT COUNT(*)  FROM users u 
+        INNER JOIN login l ON l.login_id = u.user_login_id
+        WHERE login_rank = 'Client' ";
+        $stmt = $mysqli->prepare($query);
+        $stmt->execute();
+        $stmt->bind_result($clients);
+        $stmt->fetch();
+        $stmt->close();
+
+        /* Posted Errands */
+        $query = "SELECT COUNT(*)  FROM errands";
+        $stmt = $mysqli->prepare($query);
+        $stmt->execute();
+        $stmt->bind_result($errands);
+        $stmt->fetch();
+        $stmt->close();
+
+
+        /* My Biddings */
+        $query = "SELECT COUNT(*)  FROM biddings WHERE bidding_user_id = '$freelancer_id'";
+        $stmt = $mysqli->prepare($query);
+        $stmt->execute();
+        $stmt->bind_result($biddings);
+        $stmt->fetch();
+        $stmt->close();
+
+        /* My Accepted Biddings */
+        $query = "SELECT COUNT(*)  FROM accepted_bids ab 
+        INNER JOIN biddings b ON b.bidding_id = ab.accepted_bid_id
+        WHERE b.bidding_user_id = '$freelancer_id'";
+        $stmt = $mysqli->prepare($query);
+        $stmt->execute();
+        $stmt->bind_result($accepted_biddings);
+        $stmt->fetch();
+        $stmt->close();
+
+        /* Payments */
+        $query = "SELECT SUM(payment_amount)  FROM payments p
+        INNER JOIN accepted_bids ab ON ab.accepted_bid_id = p.payment_accepted_bid_id
+        INNER JOIN biddings b ON b.bidding_id = ab.accepted_bid_bidding_id 
+        WHERE b.bidding_user_id = '$client_id' ";
+        $stmt = $mysqli->prepare($query);
+        $stmt->execute();
+        $stmt->bind_result($payments);
+        $stmt->fetch();
+        $stmt->close();
+    }
+}
