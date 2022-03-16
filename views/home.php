9de3589e538041c0ba484eb486b0031a2642313f
Martin Mbithi <martdevelopers254@gmail.com>
2022-03-16 18:37:54 +0300

    Implement Slider Icons

--- views/home.php
+++ views/home.php
@@ -36,126 +36,52 @@ require_once('../partials/head.php');
         <!-- Hero Slides-->
         <div class="owl-carousel-one owl-carousel">
             <!-- Single Hero Slide-->
-            <div class="single-hero-slide bg-overlay" style="background-image: url('img/bg-img/31.jpg')">
+            <div class="single-hero-slide bg-overlay" style="background-image: url('../public/img/bg-img/clients.webp')">
                 <div class="slide-content h-100 d-flex align-items-center text-center">
                     <div class="container">
                         <h4 class="text-white mb-1" data-animation="fadeInUp" data-delay="100ms" data-wow-duration="500ms">Clients</h4>
-                        <p class="text-white mb-4" data-animation="fadeInUp" data-delay="400ms" data-wow-duration="1000ms">Ready to hire and pay for errands services</p>
+                        <p class="text-white mb-4" data-animation="fadeInUp" data-delay="400ms" data-wow-duration="1000ms">Prepared to employ and pay for errand-running services.</p>
                         <a class="btn btn-creative btn-warning" href="clients" data-animation="fadeInUp" data-delay="800ms" data-wow-duration="500ms"><?php echo $clients; ?></a>
                     </div>
                 </div>
             </div>
             <!-- Single Hero Slide-->
-            <div class="single-hero-slide bg-overlay" style="background-image: url('img/bg-img/32.jpg')">
+            <div class="single-hero-slide bg-overlay" style="background-image: url('../public/img/bg-img/freelancers.webp')">
                 <div class="slide-content h-100 d-flex align-items-center text-center">
                     <div class="container">
                         <h4 class="text-white mb-1" data-animation="fadeInUp" data-delay="100ms" data-wow-duration="1000ms">Freelancers</h4>
-                        <p class="text-white mb-4" data-animation="fadeInUp" data-delay="400ms" data-wow-duration="1000ms">Ready and available freelancers to run your errands</p>
+                        <p class="text-white mb-4" data-animation="fadeInUp" data-delay="400ms" data-wow-duration="1000ms">Freelancers that are ready and waiting to conduct your errands.</p>
                         <a class="btn btn-creative btn-warning" href="freelancers" data-animation="fadeInUp" data-delay="800ms" data-wow-duration="500ms"><?php echo $freelancers; ?></a>
                     </div>
                 </div>
             </div>
             <!-- Single Hero Slide-->
-            <div class="single-hero-slide bg-overlay" style="background-image: url('img/bg-img/33.jpg')">
+            <div class="single-hero-slide bg-overlay" style="background-image: url('../public/img/bg-img/errands.jpg')">
                 <div class="slide-content h-100 d-flex align-items-center text-center">
                     <div class="container">
                         <h4 class="text-white mb-1" data-animation="fadeInUp" data-delay="100ms" data-wow-duration="1000ms">Posted Errands</h4>
-                        <p class="text-white mb-4" data-animation="fadeInUp" data-delay="400ms" data-wow-duration="1000ms">Readly and available errands services openings.</p>
+                        <p class="text-white mb-4" data-animation="fadeInUp" data-delay="400ms" data-wow-duration="1000ms">Openings for errands services that are both readily accessible and readily available.</p>
                         <a class="btn btn-creative btn-warning" href="errands" data-animation="fadeInUp" data-delay="800ms" data-wow-duration="500ms"><?php echo $errands; ?></a>
                     </div>
                 </div>
             </div>
             <!-- Single Hero Slide-->
-            <div class="single-hero-slide bg-overlay" style="background-image: url('img/bg-img/1.jpg')">
+            <div class="single-hero-slide bg-overlay" style="background-image: url('../public/img/bg-img/biddings.jpg')">
                 <div class="slide-content h-100 d-flex align-items-center text-center">
                     <div class="container">
                         <h4 class="text-white mb-1" data-animation="fadeInUp" data-delay="100ms" data-wow-duration="1000ms">Biddings</h4>
-                        <p class="text-white mb-4" data-animation="fadeInUp" data-delay="400ms" data-wow-duration="1000ms">Freelancers are ready to offer services on posted errands.</p>
+                        <p class="text-white mb-4" data-animation="fadeInUp" data-delay="400ms" data-wow-duration="1000ms">Freelancers are eager to help with the tasks that have been placed.</p>
                         <a class="btn btn-creative btn-warning" href="errands" data-animation="fadeInUp" data-delay="800ms" data-wow-duration="500ms"><?php echo $biddings; ?></a>
                     </div>
                 </div>
             </div>
             <!-- Single Hero Slide-->
-            <div class="single-hero-slide bg-overlay" style="background-image: url('img/bg-img/1.jpg')">
+            <div class="single-hero-slide bg-overlay" style="background-image: url('../public/img/bg-img/payments.webp')">
                 <div class="slide-content h-100 d-flex align-items-center text-center">
                     <div class="container">
                         <h4 class="text-white mb-1" data-animation="fadeInUp" data-delay="100ms" data-wow-duration="1000ms">Payments</h4>
-                        <p class="text-white mb-4" data-animation="fadeInUp" data-delay="400ms" data-wow-duration="1000ms">Freelancers are earning quite alot on this platform, Clients are spending too.</p>
+                        <p class="text-white mb-4" data-animation="fadeInUp" data-delay="400ms" data-wow-duration="1000ms">On this platform, freelancers make a lot of money, and clients spend a lot of money as well.</p>
                         <a class="btn btn-creative btn-warning" href="payments" data-animation="fadeInUp" data-delay="800ms" data-wow-duration="500ms">Ksh <?php echo number_format($payments, 2); ?></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="py-4">
-            <div class="container direction-rtl">
-                <div class="row g-3">
-                    <div class="col-3">
-                        <div class="feature-card text-center">
-                            <div class="card shadow mx-auto"><img src="img/demo-img/bootstrap.png" alt=""></div>
-                            <p class="mb-0">Bootstrap 5</p>
-                        </div>
-                    </div>
-                    <div class="col-3">
-                        <div class="feature-card mx-auto text-center">
-                            <div class="card shadow mx-auto"><img src="img/demo-img/pwa.png" alt=""></div>
-                            <p class="mb-0">PWA ready</p>
-                        </div>
-                    </div>
-                    <div class="col-3">
-                        <div class="feature-card mx-auto text-center">
-                            <div class="card shadow mx-auto"><img src="img/demo-img/sass.png" alt=""></div>
-                            <p class="mb-0">Sass </p>
-                        </div>
-                    </div>
-                    <div class="col-3">
-                        <div class="feature-card mx-auto text-center">
-                            <div class="card shadow mx-auto"><img src="img/demo-img/pug.png" alt=""></div>
-                            <p class="mb-0">Pug</p>
-                        </div>
-                    </div>
-                    <div class="col-3">
-                        <div class="feature-card mx-auto text-center">
-                            <div class="card shadow mx-auto"><img src="img/demo-img/npm.png" alt=""></div>
-                            <p class="mb-0">npm</p>
-                        </div>
-                    </div>
-                    <div class="col-3">
-                        <div class="feature-card mx-auto text-center">
-                            <div class="card shadow mx-auto"><img src="img/demo-img/gulp.png" alt=""></div>
-                            <p class="mb-0">gulp.js</p>
-                        </div>
-                    </div>
-                    <div class="col-3">
-                        <div class="feature-card mx-auto text-center">
-                            <div class="card shadow mx-auto"><i class="bi bi-box-arrow-left text-primary"></i></div>
-                            <p class="mb-0">RTL ready</p>
-                        </div>
-                    </div>
-                    <div class="col-3">
-                        <div class="feature-card mx-auto text-center">
-                            <div class="card shadow mx-auto"><i class="bi bi-moon text-dark"></i></div>
-                            <p class="mb-0">Dark mode</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="card bg-danger mb-3 shadow-sm bg-gradient direction-rtl">
-                        <div class="card-body">
-                            <h2 class="text-white">Reusable elements</h2>
-                            <p class="text-white mb-4">More than 220+ reusable modern design elements. Just copy the code and paste it on your desired page.</p><a class="btn btn-warning" href="elements.html">All elements</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-12">
-                    <div class="card bg-primary mb-3 shadow-sm bg-gradient direction-rtl">
-                        <div class="card-body">
-                            <h2 class="text-white">Ready pages</h2>
-                            <p class="text-white mb-4">Already designed more than 35+ pages for your needs. Such as - Authentication, Chats, eCommerce, Blog &amp; Miscellaneous pages.</p><a class="btn btn-info" href="pages.html">All pages</a>
-                        </div>
                     </div>
                 </div>
             </div>
