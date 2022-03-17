a87fa99133312e37e13601703b4b7cae43b2bebe
Martin Mbithi <martdevelopers254@gmail.com>
2022-03-17 16:38:27 +0300

    Implement Action Modals Outside Page Content Wrapper Class

--- views/errand_detail.php
+++ views/errand_detail.php
@@ -82,6 +82,26 @@ require_once('../partials/head.php');
                 </div>
             </div>
         </div>
+        <!-- Bid Modal -->
+        <div class="modal fade" id="bid_<?php echo $errand->errand_id; ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h6 class="modal-title" id="exampleModalLabel">Modal Heading</h6>
+                        <button class="btn btn-close p-1 ms-auto" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <p class="mb-0">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam quasi illum eveniet quidem dolores consectetur tempore architecto quo quos.</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button class="btn btn-sm btn-secondary" type="button" data-bs-dismiss="modal">Close</button>
+                        <button class="btn btn-sm btn-success" type="button">Save</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Update Modal -->
+        <!-- Delete Modal -->
         <!-- Side Nav Wrapper-->
         <?php require_once('../partials/side_nav.php'); ?>
         <div class="page-content-wrapper py-3">
@@ -89,16 +109,27 @@ require_once('../partials/head.php');
                 <div class="card product-details-card mb-3 direction-rtl border-primary">
                     <div class="card-body">
                         <h5>Errand Details</h5>
-                        <p><?php echo $errand->errand_description; ?></p>
+                        <p><?php echo $errand->errand_description; ?></p><br>
+                        <figcaption class="blockquote-footer">
+                            Posted By <cite title="Source Title"><?php echo $errand->user_fname . ' ' . $errand->user_lname; ?></cite>
+                        </figcaption>
                         <hr>
-                        <span class="text-success">
-                            Amount: Ksh<?php echo number_format($errand->errand_amount); ?><br>
-                            Due Date: <?php echo date('d M Y', strtotime($errand->errand_due_date)); ?><br>
-                            Total Bids: <?php echo $biddings; ?> <br>
-                        </span>
+                        <p>
+                            <span class="text-success">
+                                Amount: Ksh<?php echo number_format($errand->errand_amount); ?><br>
+                                Due Date: <?php echo date('d M Y', strtotime($errand->errand_due_date)); ?><br>
+                                Total Bids: <?php echo $biddings; ?> <br>
+                            </span>
+                        </p>
                     </div>
                     <div class="card-footer">
                         <div class="text-center">
+                            <button class="btn btn-round btn-success" type="button" data-bs-toggle="modal" data-bs-target="#bid_<?php echo $errand->errand_id; ?>">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-megaphone" viewBox="0 0 16 16">
+                                    <path d="M13 2.5a1.5 1.5 0 0 1 3 0v11a1.5 1.5 0 0 1-3 0v-.214c-2.162-1.241-4.49-1.843-6.912-2.083l.405 2.712A1 1 0 0 1 5.51 15.1h-.548a1 1 0 0 1-.916-.599l-1.85-3.49a68.14 68.14 0 0 0-.202-.003A2.014 2.014 0 0 1 0 9V7a2.02 2.02 0 0 1 1.992-2.013 74.663 74.663 0 0 0 2.483-.075c3.043-.154 6.148-.849 8.525-2.199V2.5zm1 0v11a.5.5 0 0 0 1 0v-11a.5.5 0 0 0-1 0zm-1 1.35c-2.344 1.205-5.209 1.842-8 2.033v4.233c.18.01.359.022.537.036 2.568.189 5.093.744 7.463 1.993V3.85zm-9 6.215v-4.13a95.09 95.09 0 0 1-1.992.052A1.02 1.02 0 0 0 1 7v2c0 .55.448 1.002 1.006 1.009A60.49 60.49 0 0 1 4 10.065zm-.657.975 1.609 3.037.01.024h.548l-.002-.014-.443-2.966a68.019 68.019 0 0 0-1.722-.082z" />
+                                </svg>
+                                Bid
+                            </button>
                             <button class="btn btn-round btn-primary" type="button" data-bs-toggle="modal" data-bs-target="#update_<?php echo $errand->errand_id; ?>">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                                     <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
@@ -117,11 +148,11 @@ require_once('../partials/head.php');
                     </div>
                 </div>
             </div>
-        <?php } ?>
-        <!-- Footer Nav-->
-        <?php require_once('../partials/footer_nav.php'); ?>
-        <?php require_once('../partials/scripts.php'); ?>
+        </div>
+    <?php } ?>
+    <!-- Footer Nav-->
+    <?php require_once('../partials/footer_nav.php'); ?>
+    <?php require_once('../partials/scripts.php'); ?>
 </body>
-
 
 </html>
