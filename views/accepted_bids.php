b0bcecaa72ed763bcb8eb5f8dc5452a76c6dca88
Martin Mbithi <martdevelopers254@gmail.com>
2022-03-21 05:58:47 +0300

    Implement Payments

--- views/accepted_bids.php
+++ views/accepted_bids.php
@@ -162,15 +162,31 @@ require_once('../partials/head.php');
                             <figcaption class="blockquote-footer">
                                 Bid By <cite title="Source Title"><?php echo $biddings->user_fname . ' ' . $biddings->user_lname; ?></cite>
                             </figcaption>
-                            <div class="text-center">
-                                <button class="btn btn-round btn-primary" type="button" data-bs-toggle="modal" data-bs-target="#pay_<?php echo $biddings->accepted_bid_id; ?>">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-                                        <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
-                                    </svg>
-                                    Pay Bid
-                                </button>
-                            </div>
+                            <?php
+                            $query = "SELECT COUNT(*)  FROM payments WHERE payment_accepted_bid_id = '$biddings->accepted_bid_id'";
+                            $stmt = $mysqli->prepare($query);
+                            $stmt->execute();
+                            $stmt->bind_result($paid_bidding);
+                            $stmt->fetch();
+                            $stmt->close();
+                            if ($paid_bidding > 0) {
+                            ?>
+                                <div class="text-center">
+                                    <button class="btn btn-round btn-success" type="button">
+                                        Already Paid
+                                    </button>
+                                </div>
+                            <?php } else { ?>
+                                <div class="text-center">
+                                    <button class="btn btn-round btn-primary" type="button" data-bs-toggle="modal" data-bs-target="#pay_<?php echo $biddings->accepted_bid_id; ?>">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                                        </svg>
+                                        Pay Bid
+                                    </button>
+                                </div>
+                            <?php } ?>
                         </div>
                     </li>
                 </ul>
