2486cf2e0dadde95d5506bab20c037359d713c38
Martin Mbithi <martdevelopers254@gmail.com>
2022-03-22 19:14:52 +0300

    Already Accept Bidding

--- views/client_errand_detail.php
+++ views/client_errand_detail.php
@@ -352,7 +352,15 @@ require_once('../partials/head.php');
                                     Bid By <cite title="Source Title"><?php echo $biddings->user_fname . ' ' . $biddings->user_lname; ?></cite>
                                 </figcaption>
                                 <div class="text-center">
-                                    <?php if ($_SESSION['user_id'] == ($biddings->errand_user_id)) { ?>
+                                    <?php
+                                    /* Dont Accept Bid Twice */
+                                    $query = "SELECT COUNT(*)  FROM accepted_bids WHERE accepted_bid_bidding_id = '$biddings->bidding_id '";
+                                    $stmt = $mysqli->prepare($query);
+                                    $stmt->execute();
+                                    $stmt->bind_result($accepted_bidding);
+                                    $stmt->fetch();
+                                    $stmt->close();
+                                    if ($_SESSION['user_id'] == ($biddings->errand_user_id) && $accepted_bidding < 0) { ?>
                                         <button class="btn btn-round btn-success" type="button" data-bs-toggle="modal" data-bs-target="#accept_bid_<?php echo $biddings->bidding_id; ?>">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard-check" viewBox="0 0 16 16">
                                                 <path fill-rule="evenodd" d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z" />
@@ -360,6 +368,15 @@ require_once('../partials/head.php');
                                                 <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z" />
                                             </svg>
                                             Accept Bid
+                                        </button>
+                                    <?php } else { ?>
+                                        <button class="btn btn-round btn-primary" type="button">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard-check" viewBox="0 0 16 16">
+                                                <path fill-rule="evenodd" d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z" />
+                                                <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z" />
+                                                <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z" />
+                                            </svg>
+                                            Already Accepted
                                         </button>
                                     <?php }
                                     /* You can delete and update your own bids */
